8421807dcd61b979e6fd67acda7fa851c7f8e813
Martin Rada <martinradadl@gmail.com>
2025-09-27 18:29:06 -0500

    books carousel improved

--- src/components/books-carousel.tsx
+++ src/components/books-carousel.tsx
@@ -13,7 +13,7 @@ export const BooksCarousel = ({ showAllLabel }: BooksCarouselProps) => {
                     .map((_, i) => (
                         <div
                             key={i}
-                            className="bg-gray-600 rounded focus:ring-3 focus:ring-offset-3 aspect-[2/3] min-w-[40%] min-[35rem]:min-w-[35%] min-[40rem]:min-w-[30%] min-[50rem]:min-w-[25%] min-[60rem]:min-w-[23.1%]"
+                            className="bg-gray-600 rounded focus:ring-3 focus:ring-offset-3 aspect-[2/3] min-w-[25%] md:min-w-[30%] lg:min-w-[23.5%]"
                             tabIndex={0}
                         />
                     ))}
