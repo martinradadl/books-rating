1ba0e260bfd670df70f29587f07f69b7eeb04669
Martin Rada <martinradadl@gmail.com>
2025-09-26 19:13:43 -0500

    BooksCarousel number of items changed based on current width

--- src/components/books-carousel.tsx
+++ src/components/books-carousel.tsx
@@ -1,27 +1,62 @@
-import { MdArrowForwardIos } from "react-icons/md"
+import { useEffect, useRef, useState } from "react";
+import { MdArrowForwardIos } from "react-icons/md";
 
 type BooksCarouselProps = {
     showAllLabel: string;
-    items?: number;
-}
+};
 
+export const BooksCarousel = ({ showAllLabel }: BooksCarouselProps) => {
+    const [itemsToShow, setItemsToShow] = useState(5);
+    const carouselRef = useRef<HTMLDivElement | null>(null);
 
-export const BooksCarousel = ({ showAllLabel, items = 5 }: BooksCarouselProps) => {
-    return (<>
-        <div className="flex gap-4 my-6">
-            {Array(items)
-                .fill(null)
-                .map((_, i) => (
-                    <div
-                        key={i}
-                        className="bg-gray-600 rounded aspect-[2/3] flex-1"
-                    />
-                ))}
+    useEffect(() => {
+        const calculateItemsToShow = () => {
+            if (carouselRef.current) {
+                const carouselWidth = carouselRef.current.offsetWidth;
+
+                if (carouselWidth < 400) setItemsToShow(2);
+                else if (carouselWidth < 600) setItemsToShow(3);
+                else if (carouselWidth < 800) setItemsToShow(4);
+                else setItemsToShow(5);
+            }
+        };
+
+        const resizeObserver = new ResizeObserver(calculateItemsToShow);
+
+        const currentCarouselRef = carouselRef.current;
+
+        if (currentCarouselRef) {
+            resizeObserver.observe(currentCarouselRef);
+        }
+
+        calculateItemsToShow();
+
+        return () => {
+            if (currentCarouselRef) {
+                resizeObserver.unobserve(currentCarouselRef);
+            }
+        };
+    }, []);
+
+    return (
+        <div ref={carouselRef}>
+            <div className="flex gap-4 my-6">
+                {Array(itemsToShow)
+                    .fill(null)
+                    .map((_, i) => (
+                        <div
+                            key={i}
+                            className="bg-gray-600 rounded aspect-[2/3] flex-1"
+                        />
+                    ))}
+            </div>
+
+            <div className="flex gap-2 items-center cursor-pointer group">
+                <p className="font-semibold group-hover:underline">
+                    {showAllLabel}
+                </p>
+                <MdArrowForwardIos size={16} />
+            </div>
         </div>
-
-        <div className="flex gap-2 items-center cursor-pointer group">
-            <p className="font-semibold group-hover:underline">{showAllLabel}</p>
-            <MdArrowForwardIos size={16} />
-        </div>
-    </>)
-}
+    );
+};
