939f8901da29ca0da508977f4c85dd38ed44840d
Martin Rada <martinradadl@gmail.com>
2025-10-10 09:50:14 -0500

    Shop book button added

--- src/components/book-actions.tsx
+++ src/components/book-actions.tsx
@@ -12,9 +12,9 @@ export const BookActions = ({ showOnMobileView }: BookActionsProps) => {
 
     return <div className={classNames("flex-col gap-4 items-center pb-4 w-full max-w-[260px] m-auto", dynamicStyles)}>
         <PillButton label="Want to Read" className="w-full h-11 bg-green-800 hover:bg-green-700 focus:ring-green-800" />
+        <PillButton label="Kindle $3.99" className="w-full h-11 !text-black bg-gray-100 border-1 border-green-800 hover:bg-gray-300 focus:ring-green-800" />
 
         <StarRating interactive />
-
         <LabelText text="Rate this book" className="cursor-pointer" />
     </div>
 }
