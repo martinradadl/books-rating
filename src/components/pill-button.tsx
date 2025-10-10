36f95441ca4083a703bcc361bcba09547aee8779
Martin Rada <martinradadl@gmail.com>
2025-10-10 14:00:14 -0500

    Cursor pointer added to pill button component

--- src/components/pill-button.tsx
+++ src/components/pill-button.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export const PillButton = ({ label, className }: Props) => {
     return (
-        <button className={classNames('bg-black text-white font-bold items-center rounded-full py-2 hover:bg-gray-600 focus:ring-3 focus:ring-black focus:ring-offset-3',
+        <button className={classNames('bg-black text-white font-bold items-center rounded-full py-2 cursor-pointer hover:bg-gray-600 focus:ring-3 focus:ring-black focus:ring-offset-3',
             className)}>
             {label}
         </button>
