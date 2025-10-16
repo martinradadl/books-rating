7473a9b2b498aea2c9c10719f4c59b82a030782e
Martin Rada <martinradadl@gmail.com>
2025-10-15 21:59:10 -0500

    Header component added to root

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Book } from "./pages/book.tsx";
+import { Header } from "./components/header.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Header />
     <Book />
   </StrictMode>
 );
