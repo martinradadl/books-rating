3d7cdf61e3e9bd5b5d7e73e02e6ee175e6c25737
Martin Rada <104103475+martinradadl@users.noreply.github.com>
2025-10-17 11:50:21 -0500

    Feat/header and footer (#4)
    * header created
    
    * join the discussion session added
    
    * Other books by the same author carrousel added, styles updated
    
    * Header component added to root
    
    * Items size on books carousel improved
    
    * Missing padding on xl breakpoint added to header
    
    * fix sticky book cover position that was partially hidden by the header when scrolling
    
    * Footer added
    
    * Join Discussion section font sizes improved
    
    * Max width added to footer, header book page
    
    * Classnames notation with conditionals improved

---
+++ src/components/discussion-options.tsx
@@ -0,0 +1,45 @@
+import { MdArrowForwardIos, MdContactSupport, MdFormatQuote, MdForum } from "react-icons/md"
+
+export const DiscussionOptions = () => {
+    const quotesCount = 3223;
+    const discussionsCount = 800;
+    const questionsCount = 233;
+
+    return (
+        <div className="flex flex-col pt-8 md:flex-row">
+            <div className="flex p-3 mb-6 items-center focus:ring-2 rounded group cursor-pointer md:flex-1" tabIndex={0}>
+                <MdFormatQuote className="text-yellow-600" size={100} />
+                <div className="flex flex-col flex-1 pl-3.5 group-hover:underline">
+                    <p className="text-[clamp(36px,4vw,64px)]">{quotesCount}</p>
+                    <div className="flex items-center">
+                        <p className="text-lg">quotes</p>
+                        <MdArrowForwardIos size={16} className="hidden md:block pl-1" />
+                    </div>
+                </div>
+                <MdArrowForwardIos size={24} className="md:hidden" />
+            </div>
+            <div className="flex p-3 mb-6 items-center focus:ring-2 rounded group cursor-pointer md:flex-1" tabIndex={0}>
+                <MdForum className="text-indigo-600" size={100} />
+                <div className="flex flex-col flex-1 pl-3.5 group-hover:underline">
+                    <p className="text-[clamp(36px,4vw,64px)]">{discussionsCount}</p>
+                    <div className="flex items-center">
+                        <p className="text-lg">discussions</p>
+                        <MdArrowForwardIos size={16} className="hidden md:block pl-1" />
+                    </div>
+                </div>
+                <MdArrowForwardIos size={24} className="md:hidden" />
+            </div>
+            <div className="flex p-3 mb-6 items-center focus:ring-2 rounded group cursor-pointer md:flex-1" tabIndex={0}>
+                <MdContactSupport className="text-rose-600" size={100} />
+                <div className="flex flex-col flex-1 pl-3.5 group-hover:underline">
+                    <p className="text-[clamp(36px,4vw,64px)]">{questionsCount}</p>
+                    <div className="flex items-center">
+                        <p className="text-lg">questions</p>
+                        <MdArrowForwardIos size={16} className="hidden md:block pl-1" />
+                    </div>
+                </div>
+                <MdArrowForwardIos size={24} className="md:hidden" />
+            </div>
+        </div>
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Book } from "./pages/book.tsx";
+import { Header } from "./components/header.tsx";
+import { Footer } from "./components/footer.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Header />
     <Book />
+    <Footer />
   </StrictMode>
 );
