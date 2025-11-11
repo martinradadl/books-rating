ffe15bbdfcb7b0cd74d75c4a2cbdc45dada0fb06
Martin Rada <martinradadl@gmail.com>
2025-11-11 16:26:46 -0500

    data structures set

---
+++ src/data-structures/index.ts
@@ -0,0 +1,46 @@
+export interface AuthorI {
+  _id: string;
+  name: string;
+  profilePic: string;
+  description: string;
+}
+
+export interface GenreI {
+  _id: string;
+  name: string;
+  description: string;
+}
+
+export interface CharacterI {
+  _id: string;
+  name: string;
+}
+
+export interface SettingI {
+  _id: string;
+  name: string;
+}
+
+export interface BookI {
+  _id: string;
+  originalTitle: string;
+  authorId: string;
+  relatedGenres: GenreI[];
+  firstPublished: Date;
+  characters: CharacterI[];
+  settings: SettingI[];
+}
+
+export interface EditionI {
+  _id: string;
+  title: string;
+  cover: string;
+  bookId: string;
+  description: string;
+  published: Date;
+  pagesCount: number;
+  format: string;
+  ISBN: string;
+  ASIN: string;
+  language: string;
+}
