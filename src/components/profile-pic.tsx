6ff882769c2d16781438baa852fe3cf8205a17a1
Martin Rada <martinradadl@gmail.com>
2025-11-19 13:48:04 -0500

    Book editions data implemented on Book Edition page

--- src/components/profile-pic.tsx
+++ src/components/profile-pic.tsx
@@ -1,3 +1,17 @@
-export const ProfilePic = () => {
-    return (<div className="w-16 h-16 rounded-full bg-gray-600 focus:ring-3 focus:ring-offset-2" tabIndex={0} />)
-}
+type ProfilePicProps = {
+    image?: string;
+};
+
+export const ProfilePic = ({ image }: ProfilePicProps) => {
+    return (
+        <div
+            className="w-16 h-16 rounded-full bg-gray-600 focus:ring-3 focus:ring-offset-2"
+            style={{
+                backgroundImage: image ? `url(${image})` : undefined,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+            }}
+            tabIndex={0}
+        />
+    );
+};
