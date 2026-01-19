376e5a4a5711ad40ff3e0a142e96980a2299177e
Martin Rada <martinradadl@gmail.com>
2026-01-19 16:30:34 -0500

    Bug on BookCover's img src

--- src/components/editions/book-cover.tsx
+++ src/components/editions/book-cover.tsx
@@ -9,7 +9,7 @@ type BookCoverProps = {
 export const BookCover = ({ className, image, withoutRoundedCorners }: BookCoverProps) => {
     return (
         <img
-            src={image}
+            src={image || undefined}
             className={classNames("aspect-2/3 bg-gray-600 shadow-md drop-shadow-[0_2px_8px_rgba(0,0,0,0.2)]", className, { "rounded-r-lg": !withoutRoundedCorners })}
         />
     );
