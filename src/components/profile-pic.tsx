7624c7d01ada4ca0b1b109bef56487e4f3dfee80
Martin Rada <martinradadl@gmail.com>
2026-01-18 22:50:43 -0500

    ProfilePic component updated for home view, hove animation added

--- src/components/profile-pic.tsx
+++ src/components/profile-pic.tsx
@@ -1,11 +1,17 @@
+import classNames from "classnames";
+
 type ProfilePicProps = {
     image?: string;
+    isHome?: boolean;
 };
 
-export const ProfilePic = ({ image }: ProfilePicProps) => {
+export const ProfilePic = ({ image, isHome }: ProfilePicProps) => {
     return (
         <div
-            className="w-16 h-16 rounded-full bg-gray-600 focus:ring-3 focus:ring-offset-2"
+            className={classNames("w-16 h-16 bg-gray-600 focus:ring-3 focus:ring-offset-2 cursor-pointer transition duration-300 hover:brightness-75",
+                { "w-[30px] h-[30px] rounded": isHome },
+                { "rounded-full": !isHome }
+            )}
             style={{
                 backgroundImage: image ? `url(${image})` : undefined,
                 backgroundSize: "cover",
