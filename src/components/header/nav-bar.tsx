d03b8bfd84a5abc4a30d35d1535f6687cd530990
Martin Rada <martinradadl@gmail.com>
2026-05-29 10:32:56 -0500

    Nav Bar on desktop home page hidden

--- src/components/header/nav-bar.tsx
+++ src/components/header/nav-bar.tsx
@@ -76,8 +76,8 @@ export const NavBar = ({ isHomePage, isXl }: NavBarProps) => {
             },
             {
                 "hidden xl:flex": isXl,
-                "xl:hidden": !isXl,
                 "lg:hidden": isHomePage,
+                "xl:hidden": !isXl || isHomePage,
             })
         }>
             {tabs.map((label, i) => {
