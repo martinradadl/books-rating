9fc13ab5e3fbc641f058330eb0c6e9353ca1cabc
Martin Rada <martinradadl@gmail.com>
2026-06-02 15:04:28 -0500

    Classname prop added to Loading component

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,8 +1,15 @@
+import classNames from "classnames";
 import { FaSpinner } from "react-icons/fa"
 
-export const Loading = () => {
+interface LoadingProps {
+    className?: string;
+}
+
+export const Loading = ({ className }: LoadingProps) => {
     return (
-        <div className="flex flex-col gap-2 justify-center items-center h-96">
+        <div className={classNames("flex flex-col gap-2 justify-center items-center",
+            !className?.includes("h-") && "h-96",
+            className)}>
             <FaSpinner size={36} className="animate-spin" />
             <p className="text-4xl font-semibold">Loading...</p>
         </div>
