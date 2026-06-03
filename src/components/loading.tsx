ae6c5b37d98cf4114feaded1971c667c28471db7
Martin Rada <martinradadl@gmail.com>
2026-06-03 15:29:33 -0500

    TwMerge installed and implemented, husky installed a prepared

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,5 +1,5 @@
-import classNames from "classnames";
 import { FaSpinner } from "react-icons/fa"
+import { twMerge } from 'tailwind-merge'
 
 interface LoadingProps {
     className?: string;
@@ -7,9 +7,8 @@ interface LoadingProps {
 
 export const Loading = ({ className }: LoadingProps) => {
     return (
-        <div className={classNames("flex flex-col gap-2 justify-center items-center",
-            !className?.includes("h-") && "h-96",
-            className)}>
+        <div className={twMerge("flex flex-col gap-2 justify-center items-center h-96", className
+        )}>
             <FaSpinner size={36} className="animate-spin" />
             <p className="text-4xl font-semibold">Loading...</p>
         </div>
