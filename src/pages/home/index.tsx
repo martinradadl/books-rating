7a4c60bf2a68f8b1c742ea379c7340c659c66693
Martin Rada <martinradadl@gmail.com>
2026-06-21 09:37:46 -0500

    Debounce added to actions called on Home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,27 +1,28 @@
-import { useDispatch } from "react-redux"
 import { HomeDesktop } from "./desktop-view"
 import { HomeMobile } from "./mobile-view"
 import { useEffect } from "react"
+import { useIsDesktop } from "../../hooks/is-desktop"
+import { useAppDispatch } from "../../redux/hooks"
 import genresActions from "../../redux/actions/genres"
+import debounce from "lodash.debounce"
 import bookListsActions from "../../redux/actions/book-lists"
 import editionsActions from "../../redux/actions/editions"
-import type { AppDispatch } from "../../redux/store"
-import { useIsDesktop } from "../../hooks/is-desktop"
 
 export const Home = () => {
-    const dispatch = useDispatch<AppDispatch>();
     const isDesktop = useIsDesktop();
+    const dispatch = useAppDispatch();
 
     useEffect(() => {
         dispatch(
-            genresActions.getAll({
+            debounce(genresActions.getAll({
                 limit: isDesktop ? 27 : 9,
                 sortBy: "occurrence",
-            })
+            }),
+                2000)
         );
-        dispatch(bookListsActions.getAll({ limit: isDesktop ? 3 : 5 }));
-        dispatch(editionsActions.getMostRatedBooks({ enableSuggestion: isDesktop, limit: isDesktop ? 4 : 10 }));
-    }, [dispatch, isDesktop]);
+        dispatch(debounce(bookListsActions.getAll({ limit: isDesktop ? 3 : 5 }), 2000));
+        dispatch(debounce(editionsActions.getMostRatedBooks({ enableSuggestion: isDesktop, limit: isDesktop ? 4 : 10 }), 2000));
+    }, [isDesktop, dispatch]);
 
     return isDesktop ? <HomeDesktop /> : <HomeMobile />;
 }
