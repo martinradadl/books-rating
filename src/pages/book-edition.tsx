842712dd506a5433f2177a3cc576a3e06092f1f8
Martin Rada <martinradadl@gmail.com>
2026-09-22 16:39:33 -0500

    Access to Author page added through Book-Edition page

--- src/pages/book-edition.tsx
+++ src/pages/book-edition.tsx
@@ -1,7 +1,12 @@
 import { useEffect, useMemo, useState } from "react";
 import { StarRating } from "../components/ratings/star-rating";
 import { MdMenuBook } from "react-icons/md";
-import { formatNumberShort, numberToLocaleString } from "../helpers/utils";
+import {
+  formatNumberShort,
+  formattedDate,
+  numberToLocaleString,
+  textToUrlSlug,
+} from "../helpers/utils";
 import { LabelText } from "../components/label-text";
 import { ExpandableContent } from "../components/expandable-content";
 import { BooksCarousel } from "../components/books-carousel";
@@ -19,8 +24,7 @@ import { DiscussionOptions } from "../components/editions/discussion-options";
 import { useAppDispatch, useAppSelector } from "../redux/hooks";
 import type { RootState } from "../redux/store";
 import editionsActions from "../redux/actions/editions";
-import { useParams } from "react-router-dom";
-import { format } from "date-fns";
+import { useNavigate, useParams } from "react-router-dom";
 import { BookActions } from "../components/editions/book-actions";
 import { TotalRatingBar } from "../components/ratings/total-rating-bar";
 import { Loading } from "../components/loading";
@@ -62,8 +66,6 @@ export const BookEdition = () => {
 
   const { author, firstPublished, relatedGenres } = book || {};
 
-  const formattedDate = (date: Date) => format(date, "MMMM dd, yyyy");
-
   const editionDetails = useMemo(
     () => [
       { label: "Format", value: `${pagesCount} pages, ${editionFormat}` },
@@ -84,7 +86,9 @@ export const BookEdition = () => {
   const [showFullAuthorDescription, setShowFullAuthorDescription] =
     useState(false);
   const [userRating, setUserRating] = useState(0);
+  const authorNameSlug = textToUrlSlug(author?.name || "");
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
   useEffect(() => {
     return () => {
@@ -292,12 +296,15 @@ export const BookEdition = () => {
           <SectionTitle name="About the author" />
 
           <div className="flex py-2 gap-4 items-center">
-            <ProfilePic image={author?.profilePic} />
+            <div onClick={() => navigate(`/author/${authorNameSlug}`)}>
+              <ProfilePic image={author?.profilePic} />
+            </div>
 
             <div className="flex flex-col flex-1 min-w-0">
               <p
                 className="w-fit font-semibold text-lg cursor-pointer hover:underline truncate focus:ring-3 rounded"
                 tabIndex={0}
+                onClick={() => navigate(`/author/${authorNameSlug}`)}
               >
                 {author?.name}
               </p>
