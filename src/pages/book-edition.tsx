492cbb74e648b1d0c341eea39fc48feb778b917b
Martin Rada <martinradadl@gmail.com>
2026-09-23 15:12:35 -0500

    navigateToAuthorPage fucntion refactorized on Book Edition

--- src/pages/book-edition.tsx
+++ src/pages/book-edition.tsx
@@ -90,7 +90,7 @@ export const BookEdition = () => {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
 
-  const navigateToAuthorPage = (slug: string) => navigate(`/author/${slug}`);
+  const navigateToAuthorPage = () => navigate(`/author/${authorNameSlug}`);
 
   useEffect(() => {
     return () => {
@@ -154,7 +154,7 @@ export const BookEdition = () => {
             <p
               className="text-4xl cursor-pointer hover:underline focus:ring-3 focus:ring-offset-2 rounded"
               tabIndex={0}
-              onClick={() => navigateToAuthorPage(authorNameSlug)}
+              onClick={navigateToAuthorPage}
             >
               {author?.name}
             </p>
@@ -299,7 +299,7 @@ export const BookEdition = () => {
           <SectionTitle name="About the author" />
 
           <div className="flex py-2 gap-4 items-center">
-            <div onClick={() => navigateToAuthorPage(authorNameSlug)}>
+            <div onClick={navigateToAuthorPage}>
               <ProfilePic image={author?.profilePic} />
             </div>
 
@@ -307,7 +307,7 @@ export const BookEdition = () => {
               <p
                 className="w-fit font-semibold text-lg cursor-pointer hover:underline truncate focus:ring-3 rounded"
                 tabIndex={0}
-                onClick={() => navigateToAuthorPage(authorNameSlug)}
+                onClick={navigateToAuthorPage}
               >
                 {author?.name}
               </p>
